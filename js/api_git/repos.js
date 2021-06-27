4878eb79c5d7813ff3d4e14e577ab2e8e7fcf146
Nicolas Antonio Smaniotto <49732409+Nicolassmaniotto@users.noreply.github.com>
2021-06-26 23:08:25 -0300

    a

--- js/api_git/repos.js
+++ js/api_git/repos.js
@@ -3,23 +3,29 @@ function json_repo(data,pos){
 
     //limpa o o component a ser prenchido
     document.getElementById(pos).innerHTML = '';
+
     let ptbr = Object;
     ptbr.linkName = "Ir ao Github";
     ptbr.description = "Descrição:";
+    ptbr.readmeButton= "Readme:";
     //percorre o json
+    document.getElementById(pos).innerHTML = `<div class="div_repo">`;
     data.forEach(element => {
         //constroe os items encontrados
         document.getElementById(pos).innerHTML +=`
         <center>
-        <hr>
+        
         <div id='${element.id}'>
         <a href='${element.html_url}'><h1>${element.name}</h1></a>
         <h2>${ptbr.description}</h2>
         <ul><li><p>${element.description}</p></li></ul>
+        <button onclick="chamada_readme('${element.name},${element.id}')"> ${ptbr.readmeButton} </button>
         </div>
+        <hr>
         </center>
         `;
         console.log(element);
                                                     
     });
+    document.getElementById(pos).innerHTML += `</div>`;
 }
