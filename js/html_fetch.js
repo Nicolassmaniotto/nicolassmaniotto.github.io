62b69bb7f6a0fb94abc044921ef0c2de809e6894
Nicolas Smaniotto <nicolasantoniosmaniotto@gmail.com>
2022-09-19 19:02:16 -0300

    refeito do zero

--- js/html_fetch.js
+++
@@ -1,6 +0,0 @@
-
-function get_html_fetch(url,pos){
-    fetch (url,{mode:'no-cors'})
-    .then(res => res.text())
-    .then(data => document.getElementById(pos).innerHTML = data);
-}
--- js/style_control/firefox_css.js
+++
@@ -1,3 +0,0 @@
-function firefox_css(){
-    
-}
